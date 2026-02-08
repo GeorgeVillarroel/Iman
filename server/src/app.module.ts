bf3d75e92d4203c1fcebb6d67e5925d5bdbd691d
GeorgeVillarroel <george.dan.villarroel@gmail.com>
2026-02-07 23:42:06 -0300

    feat: scaffold resources and enable swagger

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { BoardsModule } from './boards/boards.module';
+import { ListsModule } from './lists/lists.module';
+import { CardsModule } from './cards/cards.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    AuthModule,
+    WorkspacesModule,
+    BoardsModule,
+    ListsModule,
+    CardsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
